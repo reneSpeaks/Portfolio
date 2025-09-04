a02256795bdedfae94521c7677e78c4a5a48c3ae
René Weiberlenn <rene.weiberlenn@live.de>
2025-09-04 16:43:08 +0200

    update: exposed vite to local network, for mobile testing purposes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+	server: {
+		allowedHosts: true,
+		host: '0.0.0.0'
+	},
 	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
