f9db390255445911c5dc369ecbe94efd82243be3
René Weiberlenn <rene.weiberlenn@live.de>
2025-09-05 00:01:43 +0200

    feature: preliminar setup for case studies section

--- src/config/projects/Projects.ts
+++ src/config/projects/Projects.ts
@@ -1,8 +1,14 @@
 import musikLebenHeader from '@assets/projects/video/musiklebenintro.mp4';
 import musikLebenLogo from '@assets/projects/logo/Musikleben_Logo_uni.png';
 import musikLebenMockUp from '@assets/projects/layouts/MusikLeben.png';
+import musikLebenThumbnail from '@assets/projects/video/MusikLeben.png';
 import musikLebenDescription from '@config/projects/descriptions/ml_introduction.md?raw';
 import musikLebenInsights from '@config/projects/descriptions/ml_insights.md?raw';
+
+import portfolioLogo from '@assets/layout/logo/R_Primary.svg';
+import portfolioDescription from '@config/projects/descriptions/portfolio_introduction.md?raw';
+import portfolioInsights from '@config/projects/descriptions/portfolio_insights.md?raw';
+import portfolioThumbnail from '@assets/projects/video/Portfolio.png';
 
 export type Projects = {
 	name: string;
@@ -11,12 +17,13 @@ export type Projects = {
 	period: string;
 	role: string;
 	context: string;
-	description: string;
-	insights: string;
+	description?: string;
+	insights?: string;
 	technologies: string[];
 	demo: string;
-	header: string;
-	mockup: string;
+	header?: string;
+	mockup?: string;
+	thumbnail?: string;
 	theme: {
 		[key: string]: string;
 	};
@@ -40,6 +47,7 @@ export const Projects: Projects = [
 		demo: 'https://www.musikleben.live',
 		header: musikLebenHeader,
 		mockup: musikLebenMockUp,
+		thumbnail: musikLebenThumbnail,
 		theme: {
 			'--color-navigation': '#fcfcfc',
 			'--color-theme': '#BFB304',
@@ -73,6 +81,56 @@ export const Projects: Projects = [
 			{
 				name: 'Black',
 				hex: '#080808'
+			}
+		]
+	},
+	{
+		name: 'Portfolio Website',
+		subheader: 'A showcase of my work and projects.',
+		logo: portfolioLogo,
+		period: 'September 2025',
+		role: 'Full-Stack Web Developer',
+		context: 'Skill Showcase',
+		description: portfolioDescription,
+		insights: portfolioInsights,
+		technologies: ['React.js', 'TypeScript', 'Node.js', 'Express.js', 'PostgreSQL', 'Framer Motion'],
+		demo: 'https://www.weiberlenn.name',
+		// header: portfolioHeader,
+		// mockup: portfolioMockUp,
+		thumbnail: portfolioThumbnail,
+		theme: {
+			'--color-navigation': '#181818',
+			'--color-theme': '#0d324d',
+			'--color-theme-footer': '#0d324d',
+			'--color-theme-footer-bg': '#080808',
+			'--color-theme-header': '#181818',
+			'--color-theme-accent-100': '#208aae',
+			'--color-theme-accent-200': '#51afce',
+			'--color-theme-primary-100': '#0d324d',
+			'--color-theme-primary-200': '#124866',
+			'--color-theme-primary-300': '#175e7e',
+			'--color-theme-primary-400': '#1c7496'
+		},
+		colors: [
+			{
+				name: 'Prussian blue',
+				hex: '#0d324d'
+			},
+			{
+				name: 'Indigo dye',
+				hex: '#124866'
+			},
+			{
+				name: 'Bondi blue',
+				hex: '#208aae'
+			},
+			{
+				name: 'White',
+				hex: '#fcfcfc'
+			},
+			{
+				name: 'Eerie black',
+				hex: '#181818'
 			}
 		]
 	}
--- src/pages/sections/home/Studies.tsx
+++ src/pages/sections/home/Studies.tsx
@@ -1,12 +1,67 @@
+import { useState } from 'react';
+import Lottie from 'react-lottie';
+import { Link } from 'react-router';
 import { usePageState } from '@hooks/usePageState';
-// import { Projects } from '@config/projects/Projects';
+import { Projects } from '@config/projects/Projects';
+import type { PageState } from '@config/Page';
+import chevronData from '@assets/layout/animation/lotties/chevron-right.json';
 
 export default function Studies() {
-	const { pageState } = usePageState();
+	const { pageState, setPageState } = usePageState();
+	const [isStopped, setIsStopped] = useState(true);
+
+	const defaultLottieOptions = {
+		loop: false,
+		autoplay: true,
+		animationData: chevronData,
+		rendererSettings: {
+			fps: 60,
+			preserveAspectRatio: 'xMidYMid slice'
+		}
+	};
+
 	return (
 		<section
-			className={`${pageState !== 'Default' ? 'invisible hidden' : 'visible flex'} bg-theme-primary-100 relative z-20 flex h-auto w-full items-center justify-center py-20`}>
-			<h1>My Studies</h1>
+			className={`${pageState !== 'Default' ? 'invisible hidden' : 'visible flex'} bg-light relative z-20 flex h-auto w-full flex-col items-center justify-center py-20`}>
+			<div className="mb-30 flex flex-col items-center gap-4">
+				<h4 className="text-accent-100 font-serif text-xl font-bold">A showcase of my recent projects</h4>
+				<h2 className="text-theme text-4xl font-[1000]">Latest Works</h2>
+			</div>
+			{Projects.map((project, index) => (
+				<div
+					key={index}
+					className={`${index % 2 === 0 ? 'left-1/18' : 'right-1/18'} group shadow-primary-100 hover:shadow-accent-100 pointer-events-auto relative z-0 mb-40 flex h-126 w-full max-w-4xl flex-col justify-end gap-2 overflow-hidden border-1 pb-20 pl-20 shadow-2xl duration-300`}>
+					<img src={project.thumbnail} alt={project.name} className="absolute inset-0 h-auto w-full object-cover" />
+					<h3 className="text-light drop-shadow-primary-100 z-11 text-5xl font-[1000] drop-shadow-lg">{project.name}</h3>
+					<p className={`text-light drop-shadow-primary-100 z-11 font-serif text-lg italic drop-shadow-lg`}>
+						{project.subheader}
+					</p>
+					<Link
+						to={project.demo}
+						onClick={() => setPageState('Default' as PageState)}
+						onMouseEnter={() => setIsStopped(false)}
+						onMouseLeave={() => setIsStopped(true)}
+						className="group button bg-primary-100 text-light drop-shadow-dark hover:bg-primary-300 pointer-events-auto relative z-12 mt-4 w-fit cursor-pointer px-8 py-3 text-lg font-bold tracking-widest drop-shadow-lg duration-300 ease-in-out">
+						<span className="z-12">View Project</span>
+						<div className="absolute top-1/2 left-45 -translate-y-1/2 cursor-pointer">
+							<Lottie options={defaultLottieOptions} height={40} width={40} isStopped={isStopped} />
+						</div>
+						<div className="button-mask" />
+					</Link>
+					<div className="absolute bottom-5 left-10 flex flex-row gap-5">
+						{project.technologies.map((tech, index) => (
+							<span key={index} className="text-light drop-shadow-primary-100 z-11 font-serif text-lg italic drop-shadow-lg">
+								{tech}
+							</span>
+						))}
+					</div>
+					<div className="border-theme absolute inset-0 z-10 h-full w-full border-8" />
+					<div className="bg-dark pointer-events-none absolute inset-0 z-1 h-full w-full opacity-20" />
+					<div className="from-primary-300 transition-width absolute top-0 left-0 z-10 h-full w-full bg-gradient-to-tr from-10% to-transparent to-100% opacity-0 duration-300 ease-in group-hover:opacity-100" />
+				</div>
+			))}
+			<div className="bg-theme-accent-100 absolute z-20 h-full w-0.25 opacity-10"></div>
+			<div className="border-theme-accent-100 absolute z-20 h-full w-[80%] max-w-6xl border-x-1 opacity-10"></div>
 		</section>
 	);
 }
